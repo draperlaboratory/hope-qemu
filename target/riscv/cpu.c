b0d498c8b38ddee843841c06e9408557b0c9a212
Nathan Studer <nathan.studer@dornerworks.com>
2019-08-30 12:15:00 -0400

    Add custom validator extension to represent the PIPE
    Set it using the policy validator enabled flag.
    
    Change-Id: I58a604113aa24e91ea8b9de8a33682a02b32734d

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -27,6 +27,7 @@
 #include "qemu/error-report.h"
 #include "hw/qdev-properties.h"
 #include "migration/vmstate.h"
+#include "policy_validator.h"
 
 /* RISC-V CPU definitions */
 
@@ -408,6 +409,13 @@ static void riscv_cpu_realize(DeviceState *dev, Error **errp)
         }
 
         set_misa(env, RVXLEN | target_misa);
+    }
+
+    /* Set validator extension if requested */
+    if (policy_validator_enabled()) {
+       set_misa(env, env->misa | RVP);
+
+       qemu_printf("\nEnabling Validator Extension\n");
     }
 
     riscv_cpu_register_gdb_regs_for_features(cs);
