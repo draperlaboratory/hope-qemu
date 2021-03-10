b9538ab691bb490fd58de543e24f8edde68955fc
Philippe Mathieu-Daudé <philmd@redhat.com>
2021-03-10 19:31:23 +0100

    net/eth: Add an assert() and invert if() statement to simplify code
    To simplify the function body, invert the if() statement, returning
    earlier.
    Since we already checked there is enough data in the iovec buffer,
    simply add an assert() call to consume the bytes_read variable.
    
    Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
    Reviewed-by: Miroslav Rezanina <mrezanin@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- net/eth.c
+++ net/eth.c
@@ -415,14 +415,6 @@ _eth_get_rss_ex_dst_addr(const struct iovec *pkt, int pkt_frags,
     bytes_read = iov_to_buf(pkt, pkt_frags, ext_hdr_offset,
                             &rt_hdr, sizeof(rt_hdr));
     assert(bytes_read == sizeof(rt_hdr));
-
-    if ((rt_hdr.rtype == 2) && (rt_hdr.segleft == 1)) {
-        bytes_read = iov_to_buf(pkt, pkt_frags, ext_hdr_offset + sizeof(rt_hdr),
-                                dst_addr, sizeof(*dst_addr));
-
-    bytes_read = iov_to_buf(pkt, pkt_frags, ext_hdr_offset,
-                            &rt_hdr, sizeof(rt_hdr));
-    assert(bytes_read == sizeof(rt_hdr));
     if ((rt_hdr.rtype != 2) || (rt_hdr.segleft != 1)) {
         return false;
     }
